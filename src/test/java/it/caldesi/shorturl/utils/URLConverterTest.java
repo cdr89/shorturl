cd7d6fe865436411fc939ba33a53ae0feee88522
Domenico Rosario Caldesi <d.r.caldesi@gmail.com>
2017-02-25 18:36:39 +0100

    Update URLConverterTest.java

--- src/test/java/it/caldesi/shorturl/utils/URLConverterTest.java
+++ src/test/java/it/caldesi/shorturl/utils/URLConverterTest.java
@@ -18,33 +18,23 @@ public class URLConverterTest {
 	}
 
 	@Test
-	public void testCleanURL() {
-		String url = "http://www.google.com/";
-		String cleanedURL = URLConverter.cleanURL(url);
-
-		assertEquals("http://www.google.com", cleanedURL);
-	}
-
-	@Test
 	public void testConversion() {
 		String url1 = "http://www.google.com";
-		String cleanedURL1 = URLConverter.cleanURL(url1);
 		String shortURL1 = urlConverter.createShortURL(url1);
 		System.out.println("shortURL1: " + shortURL1);
 		String originalURL1 = urlConverter.getOriginalURL(shortURL1);
 		System.out.println("originalURL1: " + originalURL1);
 
-		assertEquals(cleanedURL1, originalURL1);
+		assertEquals(url1, originalURL1);
 		assertEquals(shortURL1, urlConverter.createShortURL(url1));
 
 		String url2 = "http://www.caldesi.it";
-		String cleanedURL2 = URLConverter.cleanURL(url2);
 		String shortURL2 = urlConverter.createShortURL(url2);
 		System.out.println("shortURL2: " + shortURL2);
 		String originalURL2 = urlConverter.getOriginalURL(shortURL2);
 		System.out.println("originalURL2: " + originalURL2);
 
-		assertEquals(cleanedURL2, originalURL2);
+		assertEquals(url2, originalURL2);
 		assertEquals(shortURL2, urlConverter.createShortURL(url2));
 
 		assertNotEquals(shortURL1, shortURL2);
